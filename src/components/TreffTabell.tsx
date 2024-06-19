107935a0f27ba10fd33b7606fae38bd3d7982d83
jonasnico <jonas.nicolaysen@nav.no>
2024-06-19 15:51:43 +0200

    bruker samme modell som backenden

--- src/components/TreffTabell.tsx
+++ src/components/TreffTabell.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Table } from "@navikt/ds-react";
-import { Oppdragsegenskaper } from "../models/Oppdragsegenskaper";
+import { Attestasjonsdata } from "../models/Attestasjonsdata";
 
 interface TreffTabellProps {
-  treffliste: Oppdragsegenskaper[];
+  treffliste: Attestasjonsdata[];
 }
 
 export const TreffTabell: React.FC<TreffTabellProps> = ({ treffliste }) => {
@@ -14,14 +14,26 @@ export const TreffTabell: React.FC<TreffTabellProps> = ({ treffliste }) => {
           <Table.HeaderCell>Oppdrags ID</Table.HeaderCell>
           <Table.HeaderCell>Faggruppe</Table.HeaderCell>
           <Table.HeaderCell>Fagsystem ID</Table.HeaderCell>
+          <Table.HeaderCell>Antall attestanter</Table.HeaderCell>
+          <Table.HeaderCell>Linje ID</Table.HeaderCell>
+          <Table.HeaderCell>Attestert</Table.HeaderCell>
+          <Table.HeaderCell>Dato vedtak FOM</Table.HeaderCell>
+          <Table.HeaderCell>Dato vedtak TOM</Table.HeaderCell>
+          <Table.HeaderCell>Status</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>
         {treffliste.map((o) => (
           <Table.Row key={o.oppdragsId}>
             <Table.DataCell>{o.oppdragsId}</Table.DataCell>
-            <Table.DataCell>{o.navnFagGruppe}</Table.DataCell>
+            <Table.DataCell>{o.navnFaggruppe}</Table.DataCell>
             <Table.DataCell>{o.fagsystemId}</Table.DataCell>
+            <Table.DataCell>{o.antAttestanter}</Table.DataCell>
+            <Table.DataCell>{o.linjeId}</Table.DataCell>
+            <Table.DataCell>{o.attestert}</Table.DataCell>
+            <Table.DataCell>{o.datoVedtakFom}</Table.DataCell>
+            <Table.DataCell>{o.datoVedtakTom}</Table.DataCell>
+            <Table.DataCell>{o.kodeStatus}</Table.DataCell>
           </Table.Row>
         ))}
       </Table.Body>
