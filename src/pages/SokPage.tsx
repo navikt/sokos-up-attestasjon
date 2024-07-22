2b728e82ebfd6e154fd8371084dd0cc70dfdc7cf
jonasnico <jonas.nicolaysen@nav.no>
2024-07-22 13:23:04 +0200

    Skal kun rutes til /treffliste hvis man får treff på søket

--- src/pages/SokPage.tsx
+++ src/pages/SokPage.tsx
@@ -2,23 +2,45 @@ import { useEffect, useState } from "react";
 import { SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useSWRConfig } from "swr";
-import { Heading } from "@navikt/ds-react";
+import { Alert, Heading } from "@navikt/ds-react";
+import ContentLoader from "../components/common/ContentLoader";
 import SokForm from "../components/form/SokForm";
 import { SokeData } from "../components/form/SokeSchema";
+import RestService from "../services/rest-service";
 import commonstyles from "../util/common-styles.module.css";
-import { retrieveSok, storeSok } from "../util/commonUtils";
+import {
+  anyOppdragExists,
+  isEmpty,
+  retrieveSok,
+  storeSok,
+} from "../util/commonUtils";
 import styles from "./SokPage.module.css";
 
 export default function SokPage() {
   const navigate = useNavigate();
   const { mutate } = useSWRConfig();
   const [sokedata, setSokedata] = useState<SokeData | undefined>(retrieveSok);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  const { treffliste, isLoading } = RestService.useFetchTreffliste(sokedata);
+  const [shouldGoToTreffliste, setShouldGoToTreffliste] =
+    useState<boolean>(false);
 
   const handleChangeSok: SubmitHandler<SokeData> = (sokedata) => {
     storeSok(sokedata);
     setSokedata(sokedata);
-    navigate("/treffliste");
+    setShouldGoToTreffliste(true);
   };
+
+  useEffect(() => {
+    if (anyOppdragExists(treffliste) && !isLoading && shouldGoToTreffliste) {
+      navigate("/treffliste");
+      setShouldGoToTreffliste(false);
+    } else if (isEmpty(treffliste) && !isLoading && shouldGoToTreffliste) {
+      setError("Ingen treff på søket");
+      setShouldGoToTreffliste(false);
+    }
+  }, [isLoading, navigate, shouldGoToTreffliste, treffliste]);
 
   useEffect(() => {
     mutate("/sok", []);
@@ -37,6 +59,8 @@ export default function SokPage() {
         </Heading>
         <SokForm sokedata={sokedata} onSubmit={handleChangeSok} />
       </div>
+      {isLoading && <ContentLoader />}
+      {error && <Alert variant="error">{error}</Alert>}
     </>
   );
 }
