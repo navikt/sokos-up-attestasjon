7a7e239c838dfce164765605f7af1696490e063f
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2024-08-22 11:49:02 +0200

    Legg til kostnadssted og ansvarssted i TreffTabell

--- src/components/treffliste/TreffTabell.tsx
+++ src/components/treffliste/TreffTabell.tsx
@@ -17,6 +17,8 @@ export const TreffTabell: React.FC<TreffTabellProps> = ({ treffliste }) => {
             <Table.HeaderCell>Faggruppe</Table.HeaderCell>
             <Table.HeaderCell>Fagsystem ID</Table.HeaderCell>
             <Table.HeaderCell>Fagområde</Table.HeaderCell>
+            <Table.HeaderCell>Kostnadssted</Table.HeaderCell>
+            <Table.HeaderCell>Ansvarssted</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -33,6 +35,8 @@ export const TreffTabell: React.FC<TreffTabellProps> = ({ treffliste }) => {
               <Table.DataCell>{oppdrag.navnFagGruppe}</Table.DataCell>
               <Table.DataCell>{oppdrag.fagsystemId}</Table.DataCell>
               <Table.DataCell>{oppdrag.navnFagOmraade}</Table.DataCell>
+              <Table.DataCell>{oppdrag.kostnadsSted}</Table.DataCell>
+              <Table.DataCell>{oppdrag.ansvarsSted}</Table.DataCell>
             </Table.Row>
           ))}
         </Table.Body>
