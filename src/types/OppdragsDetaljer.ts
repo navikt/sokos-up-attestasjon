9f22000cb1cbd9bfc6062ed43fcee652ccf3ca69
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-09-19 12:04:00 +0200

    Tilpasser detaljerskjermbildet til ny datamodell. Også nye data til treffliste, men alle de gamle feltene var der allerede

--- src/types/OppdragsDetaljer.ts
+++ src/types/OppdragsDetaljer.ts
@@ -1,26 +1,39 @@
 import { z } from "zod";
 
-export const OppdragsDetaljerSchema = z.object({
-  ansvarsStedForOppdrag: z.string().optional(),
+const OppdragslinjeSchema = z.object({
+  oppdragsLinje: z.object({
+    attestert: z.boolean(),
+    datoVedtakFom: z.string(),
+    datoVedtakTom: z.string().optional(),
+    delytelseId: z.string(),
+    kodeKlasse: z.string(),
+    linjeId: z.number(),
+    oppdragsId: z.number(),
+    sats: z.number(),
+    typeSats: z.string(),
+  }),
   ansvarsStedForOppdragsLinje: z.string().optional(),
+  kostnadsStedForOppdragsLinje: z.string().optional(),
+  attestasjoner: z.array(
+    z.object({
+      attestant: z.string().optional(),
+      datoUgyldigFom: z.string().optional(),
+    }),
+  ),
+});
+
+export const OppdragsDetaljerSchema = z.object({
   antallAttestanter: z.number(),
-  attestant: z.string().optional(),
-  datoUgyldigFom: z.string().optional(),
-  datoVedtakFom: z.string(),
-  datoVedtakTom: z.string().optional(),
-  delytelsesId: z.string(),
   fagGruppe: z.string(),
   fagOmraade: z.string(),
   fagSystemId: z.string(),
   gjelderId: z.string(),
-  kodeKlasse: z.string(),
   kodeFagOmraade: z.string(),
   kostnadsStedForOppdrag: z.string(),
-  kostnadsStedForOppdragsLinje: z.string().optional(),
-  linjeId: z.number(),
   oppdragsId: z.number(),
-  sats: z.number(),
-  satstype: z.string(),
+  ansvarsStedForOppdrag: z.string().optional(),
+  linjer: z.array(OppdragslinjeSchema),
 });
 
+export type OppdragsLinje = z.infer<typeof OppdragslinjeSchema>;
 export type OppdragsDetaljer = z.infer<typeof OppdragsDetaljerSchema>;
