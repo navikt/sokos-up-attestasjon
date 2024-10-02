e98d5addbb9831e0d1ac2608394e049caf50eeaa
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-10-02 11:16:27 +0200

    Forsøker å flytte overriden

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./designSystemOverrides.css";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
