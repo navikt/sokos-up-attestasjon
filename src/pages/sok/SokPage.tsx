139a5525473e02c512bdd1ba5bd313f3dd93631c
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2024-11-06 16:07:12 +0100

    Det skal stå søk i søkebildet
    Co-authored-by: Lars Hartvigsen <lars.hartvigsen@nav.no>

--- src/pages/sok/SokPage.tsx
+++ src/pages/sok/SokPage.tsx
@@ -155,7 +155,7 @@ export default function SokPage() {
     <>
       <div className={commonstyles.pageheading}>
         <Heading level="1" size="large" spacing>
-          Attestasjon
+          Attestasjon: Søk
         </Heading>
       </div>
       <div className={styles["sok"]}>
