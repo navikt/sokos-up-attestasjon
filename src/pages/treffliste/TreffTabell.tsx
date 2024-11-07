faee02cbf725e78945a095985277248216dba980
Lars Hartvigsen <lhartvik@gmail.com>
2024-11-07 08:56:33 +0100

    Gjør tabellen sorterbar (#84)

--- src/pages/treffliste/TreffTabell.tsx
+++ src/pages/treffliste/TreffTabell.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { Popover, Table } from "@navikt/ds-react";
+import { Popover, SortState, Table } from "@navikt/ds-react";
 import { useStore } from "../../store/AppState";
 import commonstyles from "../../styles/common-styles.module.css";
 import { OppdragList } from "../../types/Oppdrag";
@@ -14,22 +14,77 @@ export default function TreffTabell(props: TreffTabellProps) {
   const [openState, setOpenState] = useState(false);
   const [activeRow, setActiveRow] = useState<number | null>(null);
   const rowRefs = useRef<(HTMLAnchorElement | null)[]>([]);
+  const [sort, setSort] = useState<ScopedSortState | undefined>();
+
+  interface ScopedSortState extends SortState {
+    orderBy: keyof (typeof props.oppdragList)[0];
+  }
+
+  const handleSort = (sortKey: ScopedSortState["orderBy"]) => {
+    setSort(
+      sort && sortKey === sort.orderBy && sort.direction === "descending"
+        ? undefined
+        : {
+            orderBy: sortKey,
+            direction:
+              sort && sortKey === sort.orderBy && sort.direction === "ascending"
+                ? "descending"
+                : "ascending",
+          },
+    );
+  };
+
+  function comparator<T>(a: T, b: T, orderBy: keyof T): number {
+    if (b[orderBy] == null || b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortedData = props.oppdragList.slice().sort((a, b) => {
+    if (sort) {
+      return sort.direction === "ascending"
+        ? comparator(b, a, sort.orderBy)
+        : comparator(a, b, sort.orderBy);
+    }
+    return 1;
+  });
 
   return (
     <>
-      <Table>
+      <Table
+        sort={sort}
+        onSortChange={(sortKey) =>
+          handleSort(sortKey as ScopedSortState["orderBy"])
+        }
+      >
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Gjelder</Table.HeaderCell>
-            <Table.HeaderCell>Faggruppe</Table.HeaderCell>
-            <Table.HeaderCell>Fagsystem id</Table.HeaderCell>
-            <Table.HeaderCell>Fagområde</Table.HeaderCell>
-            <Table.HeaderCell>Kostnadssted</Table.HeaderCell>
-            <Table.HeaderCell>Ansvarssted</Table.HeaderCell>
+            <Table.ColumnHeader sortKey={"gjelderId"} sortable>
+              Gjelder???
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={"fagGruppe"} sortable>
+              Faggruppe
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={"fagSystemId"} sortable>
+              Fagsystem id
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={"fagOmraade"} sortable>
+              Fagområde
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={"kostnadsSted"}>
+              Kostnadssted
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortKey={"ansvarsSted"}>
+              Ansvarssted
+            </Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {props.oppdragList.map((oppdrag, index) => (
+          {sortedData.map((oppdrag, index) => (
             <Table.Row key={oppdrag.oppdragsId}>
               <Table.DataCell>
                 <Link
