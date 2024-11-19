91360f0f078459dda63da19791027b4f432c930e
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2024-11-19 13:03:35 +0100

    Valider på fagsystem id lengde

--- src/types/schema/SokeDataSchema.ts
+++ src/types/schema/SokeDataSchema.ts
@@ -15,6 +15,7 @@ const gjelderIdLengthRule: ZodEffects<ZodString, string, string> = z
 
 const fagSystemIdRule: ZodString = z
   .string()
+  .min(4, "Fagsystem id må være minst 4 tegn langt")
   .regex(
     /^[a-zæøåA-ZÆØÅ0-9-._]*$/,
     "Fagsystem id kan bare inneholde bokstaver, tall, bindestrek, punktum og understrek",
@@ -37,7 +38,7 @@ export const SokeDataSchema = z
       if (data.fagGruppe && data.attestertStatus === "false") return true;
       if (data.fagOmraade && data.attestertStatus === "false") return true;
       if (data.gjelderId?.length !== 0) return true;
-      if ((data.fagSystemId?.length ?? 0) > 4 && data.fagOmraade) return true;
+      if (data.fagSystemId?.length !== 0 && data.fagOmraade) return true;
 
       return false;
     },
