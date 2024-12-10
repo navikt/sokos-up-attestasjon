f6ab68a5b1ecca109352b6307e325714831630ba
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-12-10 13:29:09 +0100

    legg til unmai button

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+  umami: {
+    track: (event: string) => void;
+  };
+}
---
+++ src/umami/umami.ts
@@ -0,0 +1,25 @@
+export const SOK = {
+  SUBMIT: "søkeknapp trykket",
+  GJELDERID: "søk med gjelderId",
+  FAGSYSTEM: "søk med fagsystemid",
+  FAGGRUPPE: "søk ikke attesterte i faggruppe",
+  FAGOMRAADE: "søk ikke attesterte i fagområde",
+  RESET: "søk resatt",
+};
+
+export const TREFFLISTE = {
+  EMPTY: "treffliste tomt svar",
+  ERROR: "treffliste feil",
+};
+
+export const DETALJER = {
+  AAPNE_ALLE_RADER: "detaljer åpne alle rader trykket",
+  OPPDATER_TRYKKET: "detaljer oppdater trykket",
+  ATTESTERT: "detaljer attestering",
+  AVATTESTERT: "detaljer avattestering",
+  ERROR: "detaljer kall til zos feilet",
+};
+
+export function log(s: string): void {
+  window.umami.track(s);
+}
