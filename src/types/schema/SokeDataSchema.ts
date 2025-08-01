2d47407ae436ec1b0a313ae6174bd090259836fc
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-08-01 13:19:32 +0200

    :arrow_up: Zod v4 og konform til nytt z api

--- src/types/schema/SokeDataSchema.ts
+++ src/types/schema/SokeDataSchema.ts
@@ -1,36 +1,34 @@
-import { ZodEffects, ZodString, z } from "zod";
+import { z } from "zod";
 import { AttestertStatus, AttestertStatusSchema } from "./AttestertStatus";
 import { FaggruppeSchema } from "./FagGruppeSchema";
 import { FagOmraadeSchema } from "./FagOmraadeSchema";
 
-const gjelderIdRule: ZodString = z
-  .string()
-  .regex(/^[0-9]*$/, "Gjelder-feltet kan bare inneholde tall");
-
-const gjelderIdLengthRule: ZodEffects<ZodString, string, string> = z
-  .string()
-  .refine(
-    (s) => [9, 11].includes(s.length),
-    "Gjelder-feltet må inneholde et orgnummer (9 siffer) eller fødselsnummer (11 siffer)",
-  );
-
-const fagSystemIdRule: ZodString = z
-  .string()
-  .regex(
-    /^[a-zæøåA-ZÆØÅ0-9-._]*$/,
-    "Fagsystem id kan bare inneholde bokstaver, tall, bindestrek, punktum og understrek",
-  );
-
 export const SokeDataSchema = z
   .object({
-    gjelderId: z.literal("").or(gjelderIdRule.pipe(gjelderIdLengthRule)),
-    fagSystemId: z.optional(fagSystemIdRule),
-    fagGruppe: z.optional(FaggruppeSchema),
-    fagOmraade: z.optional(FagOmraadeSchema),
+    gjelderId: z.string(),
+    fagSystemId: z.string().optional(),
+    fagGruppe: FaggruppeSchema.optional(),
+    fagOmraade: FagOmraadeSchema.optional(),
     alternativer: AttestertStatusSchema,
   })
   .refine(
     (data) => {
+      if (data.gjelderId && data.gjelderId.length > 0) {
+        if (!/^[0-9\s.]*$/.test(data.gjelderId)) {
+          return false; // bare tall
+        }
+        const cleanedId = data.gjelderId.replace(/[\s.]/g, "");
+        if (![9, 11].includes(cleanedId.length)) {
+          return false; // fnr eller orgnr
+        }
+      }
+
+      if (data.fagSystemId && data.fagSystemId.length > 0) {
+        if (!/^[a-zæøåA-ZÆØÅ0-9-._]*$/.test(data.fagSystemId)) {
+          return false;
+        }
+      }
+
       if (
         data.fagGruppe &&
         [
