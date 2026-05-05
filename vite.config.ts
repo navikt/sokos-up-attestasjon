de19c2b22ce761808ce7a348aedeac21a1f4fbe4
mudasar187 <mudasar_cheema@hotmail.com>
2026-05-05 21:28:51 +0200

    chore: optimalisere prosjektet

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 /// <reference types="vitest/config" />
 
 import { resolve } from "node:path";
-import terser from "@rollup/plugin-terser";
 import react from "@vitejs/plugin-react";
 import { playwright } from "@vitest/browser-playwright";
 import { defineConfig, esmExternalRequirePlugin } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig(({ mode }) => {
 	const serverPort = process.env.PORT || 5173;
@@ -14,8 +12,8 @@ export default defineConfig(({ mode }) => {
 	return {
 		base: "/attestasjon",
 		build: {
-			rollupOptions: {
-				input: resolve(__dirname, "src/App.tsx"),
+			rolldownOptions: {
+				input: resolve(import.meta.dirname, "src/App.tsx"),
 				preserveEntrySignatures: "exports-only",
 				plugins: [
 					esmExternalRequirePlugin({
@@ -100,13 +98,6 @@ export default defineConfig(({ mode }) => {
 				}),
 			},
 		},
-		plugins: [
-			react(),
-			cssInjectedByJsPlugin(),
-			EnvironmentPlugin({
-				NODE_ENV: process.env.NODE_ENV || "development",
-			}),
-			terser(),
-		],
+		plugins: [react(), cssInjectedByJsPlugin()],
 	};
 });
